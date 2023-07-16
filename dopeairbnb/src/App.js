6c1f4573f53bde96c43b699b91d500d38346eaa6
Raziel Oren <raziel.oren@gmail.com>
2023-07-16 14:22:36 +0530

    Dope airbnb app 2/2

--- dopeairbnb/src/App.js
+++ dopeairbnb/src/App.js
@@ -1,11 +1,32 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Card from './components/Card'
+
+import data from './data'
 
 export default function App() {
+    const cards = data.map(card => {
+        return (
+            <Card
+                id={card.id}
+                title={card.title}
+                description={card.description}
+                coverImg={card.coverImg}
+                rating={card.stats.rating}
+                reviewCount={card.stats.reviewCount}
+                location={card.location}
+                price={card.price}
+                openSpots={card.openSpots}
+            />
+        )
+    })
     return (
         <div>
             <Navbar/>
             <Hero/>
+            <div className="card-list">
+                {cards}
+            </div>
         </div>
     )
 }
